d179bfc3e73f9abaf99be996660e9a7654408160
nanashi-byakuren <ultimatebrowser.firefox@gmail.com>
2021-01-27 22:33:11 +0900

    スレ一覧を出す

--- timemachine/timemachine.py
+++ timemachine/timemachine.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from os import environ
+from os.path import dirname, join
+from pathlib import Path
 
+import praw
+from dotenv import load_dotenv
+
+dotenv_path = join(Path(dirname(__file__)).parent, '.env')
+load_dotenv(dotenv_path)
+
+client_id = environ.get("CLIENT_ID")
+client_secret = environ.get("CLIENT_SECRET")
+user_agent = environ.get("USER_AGENT")
+username = environ.get("USERNAME")
+password = environ.get("PASSWORD")
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     username=username,
+                     password=password)
+
+# 私は誰
+print(reddit.user.me())
+
+# ホットなスレを出す
+for s in reddit.subreddit('newsokur').hot(limit=20):
+    print(f"title: {s.title}, score: {s.score}")
