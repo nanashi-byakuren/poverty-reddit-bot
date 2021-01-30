ae4da77f4e70295c849ad0f17ceba0b037dd68b0
nanashi-byakuren <ultimatebrowser.firefox@gmail.com>
2021-01-31 00:54:49 +0900

    sitemapから最終更新日時を取得するように変更

--- timemachine/lastmod/spiders/lastmod_spider.py
+++ timemachine/lastmod/spiders/lastmod_spider.py
@@ -1,17 +1,24 @@
 import logging
 from typing import List
+from urllib.parse import urlparse, ParseResult
 
+import requests
+import xmltodict as xmltodict
 from dict_digger import dig
 from praw.models import Submission
 from scrapy import Spider, Request
 
 
 class LastmodSpider(Spider):
+
     name = "lastmod"
 
     def __init__(self, *args, **kwargs):
         super(LastmodSpider, self).__init__(*args, **kwargs)
         self.submissions: List[Submission] = []
+
+    def parse(self, response, **kwargs):
+        pass
 
     def start_requests(self):
         assert dig(self.settings.attributes, 'SUBMISSIONS'), "submissionが１つもないです"
@@ -26,6 +33,27 @@ class LastmodSpider(Spider):
             yield request
 
     def parse_lastmod(self, response, content_dict: dict):
-        last_modified = response.headers.get('Last-Modified')
-        submission: Submission = content_dict['submission']
-        logging.info(f"{submission.title}, last_modified {last_modified}")
+        # ヘッダーにある Last-Modifiedで判断（→これはあまり取得できない）
+        last_modified: str = response.headers.get('Last-Modified')
+        s: Submission = content_dict['submission']
+        if last_modified:
+            self.show_lastmod(s, last_modified)
+        # sitemapにある更新日付を見る
+        parse_result: ParseResult = urlparse(s.url)
+        sitemap_url = f"{parse_result.scheme}://{parse_result.netloc}/sitemap.xml"
+        last_modified = self.parse_sitemap(sitemap_url, s.url)
+        if last_modified:
+            self.show_lastmod(s, last_modified)
+
+    @staticmethod
+    def parse_sitemap(sitemap_url: str, target_url: str) -> str:
+        res = requests.get(sitemap_url)
+        raw = xmltodict.parse(res.text)
+
+        for url_dict in raw['urlset']['url']:
+            if url_dict["loc"] == target_url:
+                return url_dict["lastmod"]
+
+    @staticmethod
+    def show_lastmod(s: Submission, last_modified: str):
+        logging.info(f"{s.title}, url: {s.url}, last_modified {last_modified}")
